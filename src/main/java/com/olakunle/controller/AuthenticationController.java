43880b6efccd2647fbb16f7561a83543da5e9ed7
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-08-10 13:13:34 +0100

    optimized the whole repo

--- src/main/java/com/olakunle/controller/AuthenticationController.java
+++ src/main/java/com/olakunle/controller/AuthenticationController.java
@@ -24,7 +24,7 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
-    @PostMapping("/autheticate")
+    @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate (@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
--- src/main/java/com/olakunle/entity/Role.java
+++ src/main/java/com/olakunle/entity/Role.java
@@ -1,5 +1,5 @@
 package com.olakunle.entity;
 
 public enum Role {
-    USER, ADMIN;
+    USER, ADMIN
 }
